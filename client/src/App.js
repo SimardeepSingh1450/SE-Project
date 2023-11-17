3de35f7f8a830478a6accdaea85ef6d0452f38b3
Jaskaran Singh <jas2002karan@gmail.com>
2023-11-17 16:50:10 +0530

    fixed friends page and added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -6,19 +6,21 @@ import {Routes,Route} from 'react-router-dom';
 import Notifications from './components/Notifications/Notifications';
 import Dashboard from './components/Dashboard/Dashboard';
 import Profile from './components/ProfilePage/profile';
-import GamePage from './components/GamePage/GamePage';
+import FriendsPageNew from './components/FriendsPageNew/FriendsPage';
+import ButtonAppBar from './components/Navbar/navbar';
 
 function App() {
   return (
     <div className="App">
+      <ButtonAppBar />
       <Routes>
       <Route path="/dashboard" element={<Dashboard/>}/>
       <Route path="/loginPage" element={<LoginPage/>}/>
       <Route path="/signUpPage" element={<SignUpPage/>}/>
       <Route path="/friendsPage" element={<FriendsPage/>}/>
+      <Route path="/friendsPageNew" element={<FriendsPageNew/>}/>
       <Route path="/notificationsPage" element={<Notifications/>}/>
       <Route path="/profile" element={<Profile/>}/>
-      <Route path="/gamePage" element={<GamePage/>}/>
       </Routes>
     </div>
   );
